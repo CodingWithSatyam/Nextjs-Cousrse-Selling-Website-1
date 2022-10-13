55bf9c412fc64f96714e19f02c83eb84cae03020
templification <codeloud1@gmail.com>
2022-10-13 07:41:26 +0545

    update

--- components/Card.js
+++ components/Card.js
@@ -12,8 +12,8 @@ const Card = () => {
                         <h1 className='bg-pink-500 text-2xl font-medium text-white'>HTML Course</h1>
                         <p className='text-gray-300 m-1'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis, veritatis!</p>
                         <div className="btn flex justify-between m-5">
-                            <a className='bg-white px-7 rounded-md text-xl font-medium hover:text-pink-600 p-2 mr-2' href="/">Save</a>
-                            <a className='bg-white px-7 rounded-lg text-xl font-bold hover:text-pink-500 p-2' href="/">Buy Now!</a>
+                            <a className='bg-white px-7 rounded-md text-xl font-medium hover:text-pink-600 p-2 mr-2'>Save</a>
+                            <a className='bg-white px-7 rounded-lg text-xl font-bold hover:text-pink-500 p-2'>Buy Now!</a>
                         </div>
                     </div>
                     <div className="col lg:w-[30%] md:w-[40%] w-[90%] bg-pink-600 m-5 rounded-md">
@@ -21,8 +21,8 @@ const Card = () => {
                         <h1 className='bg-pink-500 text-2xl font-medium text-white'>HTML Course</h1>
                         <p className='text-gray-300 m-1'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis, veritatis!</p>
                         <div className="btn flex justify-between m-5">
-                            <a className='bg-white px-7 rounded-md text-xl font-medium hover:text-pink-600 p-2 mr-2' href="/">Save</a>
-                            <a className='bg-white px-7 rounded-lg text-xl font-bold hover:text-pink-500 p-2' href="/">Buy Now!</a>
+                            <a className='bg-white px-7 rounded-md text-xl font-medium hover:text-pink-600 p-2 mr-2'>Save</a>
+                            <a className='bg-white px-7 rounded-lg text-xl font-bold hover:text-pink-500 p-2'>Buy Now!</a>
                         </div>
                     </div>
                     <div className="col lg:w-[30%] md:w-[40%] w-[90%] bg-pink-600 m-5 rounded-md">
@@ -30,8 +30,8 @@ const Card = () => {
                         <h1 className='bg-pink-500 text-2xl font-medium text-white'>HTML Course</h1>
                         <p className='text-gray-300 m-1'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis, veritatis!</p>
                         <div className="btn flex justify-between m-5">
-                            <a className='bg-white px-7 rounded-md text-xl font-medium hover:text-pink-600 p-2 mr-2' href="/">Save</a>
-                            <a className='bg-white px-7 rounded-lg text-xl font-bold hover:text-pink-500 p-2' href="/">Buy Now!</a>
+                            <a className='bg-white px-7 rounded-md text-xl font-medium hover:text-pink-600 p-2 mr-2'>Save</a>
+                            <a className='bg-white px-7 rounded-lg text-xl font-bold hover:text-pink-500 p-2'>Buy Now!</a>
                         </div>
                     </div>
                     <div className="col lg:w-[30%] md:w-[40%] w-[90%] bg-pink-600 m-5 rounded-md">
@@ -39,8 +39,8 @@ const Card = () => {
                         <h1 className='bg-pink-500 text-2xl font-medium text-white'>HTML Course</h1>
                         <p className='text-gray-300 m-1'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis, veritatis!</p>
                         <div className="btn flex justify-between m-5">
-                            <a className='bg-white px-7 rounded-md text-xl font-medium hover:text-pink-600 p-2 mr-2' href="/">Save</a>
-                            <a className='bg-white px-7 rounded-lg text-xl font-bold hover:text-pink-500 p-2' href="/">Buy Now!</a>
+                            <a className='bg-white px-7 rounded-md text-xl font-medium hover:text-pink-600 p-2 mr-2'>Save</a>
+                            <a className='bg-white px-7 rounded-lg text-xl font-bold hover:text-pink-500 p-2'>Buy Now!</a>
                         </div>
                     </div>
                     <div className="col lg:w-[30%] md:w-[40%] w-[90%] bg-pink-600 m-5 rounded-md">
@@ -48,8 +48,8 @@ const Card = () => {
                         <h1 className='bg-pink-500 text-2xl font-medium text-white'>HTML Course</h1>
                         <p className='text-gray-300 m-1'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis, veritatis!</p>
                         <div className="btn flex justify-between m-5">
-                            <a className='bg-white px-7 rounded-md text-xl font-medium hover:text-pink-600 p-2 mr-2' href="/">Save</a>
-                            <a className='bg-white px-7 rounded-lg text-xl font-bold hover:text-pink-500 p-2' href="/">Buy Now!</a>
+                            <a className='bg-white px-7 rounded-md text-xl font-medium hover:text-pink-600 p-2 mr-2'>Save</a>
+                            <a className='bg-white px-7 rounded-lg text-xl font-bold hover:text-pink-500 p-2'>Buy Now!</a>
                         </div>
                     </div>
                     <div className="col lg:w-[30%] md:w-[40%] w-[90%] bg-pink-600 m-5 rounded-md">
@@ -57,8 +57,8 @@ const Card = () => {
                         <h1 className='bg-pink-500 text-2xl font-medium text-white'>HTML Course</h1>
                         <p className='text-gray-300 m-1'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis, veritatis!</p>
                         <div className="btn flex justify-between m-5">
-                            <a className='bg-white px-7 rounded-md text-xl font-medium hover:text-pink-600 p-2 mr-2' href="/">Save</a>
-                            <a className='bg-white px-7 rounded-lg text-xl font-bold hover:text-pink-500 p-2' href="/">Buy Now!</a>
+                            <a className='bg-white px-7 rounded-md text-xl font-medium hover:text-pink-600 p-2 mr-2'>Save</a>
+                            <a className='bg-white px-7 rounded-lg text-xl font-bold hover:text-pink-500 p-2'>Buy Now!</a>
                         </div>
                     </div>
                     <div className="col lg:w-[30%] md:w-[40%] w-[90%] bg-pink-600 m-5 rounded-md">
@@ -66,8 +66,8 @@ const Card = () => {
                         <h1 className='bg-pink-500 text-2xl font-medium text-white'>HTML Course</h1>
                         <p className='text-gray-300 m-1'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis, veritatis!</p>
                         <div className="btn flex justify-between m-5">
-                            <a className='bg-white px-7 rounded-md text-xl font-medium hover:text-pink-600 p-2 mr-2' href="/">Save</a>
-                            <a className='bg-white px-7 rounded-lg text-xl font-bold hover:text-pink-500 p-2' href="/">Buy Now!</a>
+                            <a className='bg-white px-7 rounded-md text-xl font-medium hover:text-pink-600 p-2 mr-2'>Save</a>
+                            <a className='bg-white px-7 rounded-lg text-xl font-bold hover:text-pink-500 p-2'>Buy Now!</a>
                         </div>
                     </div>
                     <div className="col lg:w-[30%] md:w-[40%] w-[90%] bg-pink-600 m-5 rounded-md">
@@ -75,8 +75,8 @@ const Card = () => {
                         <h1 className='bg-pink-500 text-2xl font-medium text-white'>HTML Course</h1>
                         <p className='text-gray-300 m-1'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis, veritatis!</p>
                         <div className="btn flex justify-between m-5">
-                            <a className='bg-white px-7 rounded-md text-xl font-medium hover:text-pink-600 p-2 mr-2' href="/">Save</a>
-                            <a className='bg-white px-7 rounded-lg text-xl font-bold hover:text-pink-500 p-2' href="/">Buy Now!</a>
+                            <a className='bg-white px-7 rounded-md text-xl font-medium hover:text-pink-600 p-2 mr-2'>Save</a>
+                            <a className='bg-white px-7 rounded-lg text-xl font-bold hover:text-pink-500 p-2'>Buy Now!</a>
                         </div>
                     </div>
                     <div className="col lg:w-[30%] md:w-[40%] w-[90%] bg-pink-600 m-5 rounded-md">
@@ -84,8 +84,8 @@ const Card = () => {
                         <h1 className='bg-pink-500 text-2xl font-medium text-white'>HTML Course</h1>
                         <p className='text-gray-300 m-1'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis, veritatis!</p>
                         <div className="btn flex justify-between m-5">
-                            <a className='bg-white px-7 rounded-md text-xl font-medium hover:text-pink-600 p-2 mr-2' href="/">Save</a>
-                            <a className='bg-white px-7 rounded-lg text-xl font-bold hover:text-pink-500 p-2' href="/">Buy Now!</a>
+                            <a className='bg-white px-7 rounded-md text-xl font-medium hover:text-pink-600 p-2 mr-2'>Save</a>
+                            <a className='bg-white px-7 rounded-lg text-xl font-bold hover:text-pink-500 p-2'>Buy Now!</a>
                         </div>
                     </div>
                 </div>
